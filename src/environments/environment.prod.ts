1f7f9ab3b79522160408ff5691e78d2cd03b1299
Belen Romero <romero.abelen@gmail.com>
2023-02-16 12:26:21 -0300

    deploy railway

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // URL: 'https://portfolio-belen.herokuapp.com',
-  URL: 'https://backend-portfolio-production-f3c3.up.railway.app',
+  URL: 'https://creepy-debt-production.up.railway.app',
 };
